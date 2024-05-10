29817b3e49d9786aad4f4ea47929d8b76ea91b39
Junhyeok Yoon <junhyeokyoon3@gmail.com>
2024-05-10 11:58:58 +0900

    edit files

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # CRUD Read all
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+   # Create
+  # One page for JUST the form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # Another action to create the instance from the form data
+  post '/tasks', to: 'tasks#create'
+
+   # Read one
+   get '/tasks/:id', to: 'tasks#show', as: :task
+
+    # Update
+  # One page for JUST the form
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # Another action to update the instance from the form data
+  patch '/tasks/:id', to: 'tasks#update'
+
+   # Delete
+   delete '/tasks/:id', to: 'tasks#destroy'
 end
